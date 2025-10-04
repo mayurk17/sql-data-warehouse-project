987145e9028a7cafed8795b3cd6a9faa35623839
mayurk17 <mayurk1703@gmail.com>
2025-10-04 21:42:39 +0530

    Update create_DB_schemas.sql

--- Scirpts/Bronze/create_DB_schemas.sql
+++ Scirpts/Bronze/create_DB_schemas.sql
@@ -1,10 +1,34 @@
---Creat a DB DataWarehouse
-CREATE DATABASE DataWarehouse
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse' . 
+    The script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+	
+WARNING:
+    Running this script will drop the entire 'DataWarehouse' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
 
---Difine Schemas 
+USE master;
+GO
 
-CREATE SCHEMA Bronze;
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
 GO
-CREATE SCHEMA Silver;
+
+USE DataWarehouse;
 GO
-CREATE SCHEMA Gold;
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
